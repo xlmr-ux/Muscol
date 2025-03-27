79cd518669a621f07f962763a85a43f5223f072c
xlmr-ux <82855367+xlmr-ux@users.noreply.github.com>
2025-03-27 15:38:29 +0300

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,10 @@
 import svelte from 'rollup-plugin-svelte';
+import { babel } from '@rollup/plugin-babel'; // Updated babel plugin
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
 import { terser } from 'rollup-plugin-terser';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
-import babel from 'rollup-plugin-babel';  
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -39,44 +39,24 @@ export default {
     },
     plugins: [
         svelte({
-            // enable run-time checks when not in production
             dev: !production,
-            // we'll extract any component CSS out into
-            // a separate file - better for performance
             css: css => {
                 css.write('bundle.css');
             }
         }),
-
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration -
-        // consult the documentation for details:
-        // https://github.com/rollup/plugins/tree/master/packages/commonjs
         resolve({
             browser: true,
             dedupe: ['svelte']
         }),
         commonjs(),
         nodePolyfills(),
-        // production && 
         babel({
-            exclude: [
-                // 'node_modules/**',
-                /\/core-js\//,
-            ],
+            babelHelpers: 'bundled', // Required for @rollup/plugin-babel
+            exclude: [/\/core-js\//],
             extensions: ['.svelte', '.js', '.jsx', '.es6', '.es', '.mjs', '.ts']
         }),
-        // In dev mode, call `npm run start` once
-        // the bundle has been generated
         !production && serve(),
-
-        // Watch the `public` directory and refresh the
-        // browser on changes when not in production
         !production && livereload('public'),
-
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
         production && terser()
     ],
     watch: {
